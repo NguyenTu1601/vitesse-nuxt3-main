31f8a865af5aa77a747d3181f18408091ece212e
NguyenTu1601 <nguyenvantu.qng1234@gmail.com>
2022-11-11 13:45:57 +0700

    sdfa

---
+++ pages/meta/[id].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const route = useRoute()
+console.log(route.query.t)
+console.log(route.query.d)
+console.log(route.query.i)
+
+const t = ref(route.query.t)
+const d = ref(route.query.d)
+const i = ref(route.query.i)
+
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'title', content: "LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH" },
+    { name: 'description', content: 'LIVESHOW THE BEST OF NHƯ QUỲNH là cột mốc đánh dấu 30 năm ca khúc người tình mùa đông' },
+  ],
+  link: [
+    {
+      rel: 'image',
+      href: 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/25/829FD9.jpg'
+    },
+  ]
+})
+</script>
+
+<template>
+    page
+</template>
---
+++ pages/meta/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+const router = useRouter()
+const p1 = () => {
+    router.push({
+        path: '/meta/1',
+        query: { 
+            t: 'a', 
+            d: "b", 
+            i:"c" 
+        },
+    })
+}
+</script>
+
+<template>
+    <!-- <NuxtLink to="/meta/1?t=sdf&d=das">
+        Page 1
+    </NuxtLink> -->
+    <div @click="p1">page 1</div>
+</template>
