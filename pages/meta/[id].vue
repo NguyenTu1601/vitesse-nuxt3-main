05173c52035d52bffd0dd8fcc356838dbf68896d
NguyenTu1601 <nguyenvantu.qng1234@gmail.com>
2022-11-11 15:09:25 +0700

    dsfds

--- pages/meta/[id].vue
+++ pages/meta/[id].vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
+import { json } from 'stream/consumers';
+
 const route = useRoute()
 console.log(route.query.t)
 console.log(route.query.d)
 console.log(route.query.i)
 
-const t = ref(route.query.t)
-const d = ref(route.query.d)
-const i = ref(route.query.i)
-
 useHead({
   title: 'My App',
   meta: [
     { property: 'og:title', content: "LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH"},
-    { property: 'og:description', content: "LIVESHOW THE BEST OF NHƯ QUỲNH là cột mốc đánh dấu 30 năm ca khúc người tình mùa đông" },
+    { property: 'og:description', content: JSON.stringify(route.query.t)},
     { property: 'og:image', content: 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/25/829FD9.jpg' },
 
   ],
