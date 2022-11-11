1bedabd3faf3c32e94c139a04b4821632e0bacba
NguyenTu1601 <nguyenvantu.qng1234@gmail.com>
2022-11-11 14:12:24 +0700

    dsfds

--- pages/meta/[id].vue
+++ pages/meta/[id].vue
@@ -14,6 +14,7 @@ useHead({
     { name: 'og:title', content: "LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH" },
     { name: 'og:description', content: 'LIVESHOW THE BEST OF NHƯ QUỲNH là cột mốc đánh dấu 30 năm ca khúc người tình mùa đông' },
     { name: 'og:image', content: 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/25/829FD9.jpg' },
+    { property: 'og:image', content: 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/25/829FD9.jpg' },
   ],
   link: [
     {
