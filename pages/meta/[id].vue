6df6d52903c301e42f2952a6d3aa5518e9993e17
NguyenTu1601 <nguyenvantu.qng1234@gmail.com>
2022-11-11 15:12:43 +0700

    dsfds

--- pages/meta/[id].vue
+++ pages/meta/[id].vue
@@ -9,8 +9,10 @@ console.log(route.query.i)
 useHead({
   title: 'My App',
   meta: [
+    { name: 'description', content: "LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH"},
+    { name: 'keyword', content: "LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH"},
     { property: 'og:title', content: "LIVESHOW THE BEST OF NHƯ QUỲNH | 12.11.2022 | NHÀ HÁT HOÀ BÌNH"},
-    { property: 'og:description', content: JSON.stringify(route.query.t)},
+    { property: 'og:description', content: 'LIVESHOW THE BEST OF NHƯ QUỲNH là cột mốc đánh dấu 30 năm ca khúc người tình mùa đông'},
     { property: 'og:image', content: 'https://images.tkbcdn.com/1/1560/600/Upload/eventcover/2022/10/25/829FD9.jpg' },
 
   ],
